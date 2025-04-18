e0a9cf193ea469e245ab92cb91447bfcef39474d
Zhongqi Wang <60317828+Robin-WZQ@users.noreply.github.com>
2025-04-18 13:34:40 +0800

    Add files via upload

---
+++ metric_calculate.sh
@@ -0,0 +1,64 @@
+# BadT2I
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/test/BadT2I/test_data_1.txt' --npy_save_path './data/Attention_maps/test' --metric_save_path './data/Metrics/test'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/test/BadT2I/test_data_2.txt' --npy_save_path './data/Attention_maps/test' --metric_save_path './data/Metrics/test'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/BadT2I/train_data_1.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/BadT2I/train_data_2.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/BadT2I/train_data_3.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/BadT2I/train_data_4.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train' 
+
+# EvilEdit
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/test/EvilEdit/test_data_1.txt' --npy_save_path './data/Attention_maps/test' --metric_save_path './data/Metrics/test'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/test/EvilEdit/test_data_2.txt' --npy_save_path './data/Attention_maps/test' --metric_save_path './data/Metrics/test'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/EvilEdit/train_data_1.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/EvilEdit/train_data_2.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/EvilEdit/train_data_3.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/EvilEdit/train_data_4.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+# IBA
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/test/IBA/test_data_1.txt' --npy_save_path './data/Attention_maps/test' --metric_save_path './data/Metrics/test'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/test/IBA/test_data_2.txt' --npy_save_path './data/Attention_maps/test' --metric_save_path './data/Metrics/test'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/IBA/train_data_1.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/IBA/train_data_2.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/IBA/train_data_3.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/IBA/train_data_4.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+# Rickrolling
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/test/Rickrolling/test_data_1.txt' --npy_save_path './data/Attention_maps/test' --metric_save_path './data/Metrics/test'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/test/Rickrolling/test_data_2.txt' --npy_save_path './data/Attention_maps/test' --metric_save_path './data/Metrics/test'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/Rickrolling/train_data_1.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/Rickrolling/train_data_2.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/Rickrolling/train_data_3.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data './data/Prompts/train/Rickrolling/train_data_4.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+# Villan
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data  './data/Prompts/test/Villan/test_data_1.txt' --npy_save_path './data/Attention_maps/test' --metric_save_path './data/Metrics/test'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data  './data/Prompts/test/Villan/test_data_2.txt' --npy_save_path './data/Attention_maps/test' --metric_save_path './data/Metrics/test'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data  './data/Prompts/train/Villan/train_data_1.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data  './data/Prompts/train/Villan/train_data_2.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data  './data/Prompts/train/Villan/train_data_3.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
+
+CUDA_VISIBLE_DEVICES=0 python metric_calculate.py --data  './data/Prompts/train/Villan/train_data_4.txt' --npy_save_path './data/Attention_maps/train' --metric_save_path './data/Metrics/train'
